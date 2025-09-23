d6072e779ce61ccc9ea4b59daf1ee216e0c55301
Bia Astle <astlebia@gmail.com>
2025-09-23 17:20:34 -0300

    Inicializando spring boot

---
+++ src/main/java/com/conexao_brennand/conexao_brennad/ConexaoBrennadApplication.java
@@ -0,0 +1,13 @@
+package com.conexao_brennand.conexao_brennad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoBrennadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoBrennadApplication.class, args);
+	}
+
+}
