88e437b2e29f105404b4130a4fa22dbf4c4602c5
Bia Astle <astlebia@gmail.com>
2025-09-29 23:41:00 -0300

    Estrutura inicial controller de feedback

--- src/main/java/com/conexao_brennand/conexao_brennand/service/FeedbackService.java
+++ src/main/java/com/conexao_brennand/conexao_brennand/service/FeedbackService.java
@@ -1,12 +1,30 @@
 package com.conexao_brennand.conexao_brennand.service;
 
+import com.conexao_brennand.conexao_brennand.model.Feedback;
 import com.conexao_brennand.conexao_brennand.repository.FeedbackRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class FeedbackService {
     private final FeedbackRepository feedbackRepository;
     public FeedbackService (FeedbackRepository feedbackRepository){
         this.feedbackRepository = feedbackRepository;
+    }
+    public Feedback criarFeedback(Feedback feedback){
+        return feedbackRepository.save(feedback);
+    }
+    public List<Feedback> listarFeedbacks(){
+        return feedbackRepository.findAll();
+    }
+    public Feedback buscarFeedbackId(long id){
+        return feedbackRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Feedback não encontrado"
+                ));
     }
 }
