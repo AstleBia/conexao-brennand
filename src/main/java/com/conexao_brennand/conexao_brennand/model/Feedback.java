5ab826ee1cfb7e672aa3ddbf58b48a8eb85a5377
Bia Astle <astlebia@gmail.com>
2025-09-29 23:26:25 -0300

    Adiciona anotacoes lombok

--- src/main/java/com/conexao_brennand/conexao_brennand/model/Feedback.java
+++ src/main/java/com/conexao_brennand/conexao_brennand/model/Feedback.java
@@ -1,8 +1,17 @@
 package com.conexao_brennand.conexao_brennand.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name="feedback")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Feedback {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
